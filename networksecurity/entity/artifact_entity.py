65d64d2fbb01ebdb942f7735bb9b12e20ed54387
Manasa Gonuguntla <manugonuguntla29@gmail.com>
2024-08-18 00:52:53 -0500

    Entity folder structure updated with Config and Artifact files

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    pass
+
+@dataclass
+class DataValidationArtifact:
+    pass
+
+@dataclass
+class DataTransformationArtifact:
+    pass
+
+@dataclass
+class ModelTrainerArtifact:
+    pass
+
+@dataclass
+class ModelEvaluatorArtifact:
+    pass
+
+@dataclass
+class ModelPusherArtifact:
+    pass
---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import os
+from networksecurity.constant import training_pipeline
+
+
+class TrainingPipelineConfig(self):
+    def __init__(self):
+        pass
+    
+
+class DataIngestionConfig(self):
+    def __init__(self):
+        pass
+
+class DataValidationConfig(self):
+    def __init__(self):
+        pass
+    
+
+class DataTransformationConfig(self):
+    def __init__(self):
+        pass
+
+class ModelTrainerConfig(self):
+    def __init__(self):
+        pass
+    
+
+class ModelEvaluationConfig(self):
+    def __init__(self):
+        pass
+
+class ModelPusherConfig(self):
+    def __init__(self):
+        pass
+
