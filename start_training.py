be4550932bcbb2837cea1bc314e0eb165cf5386e
Manasa Gonuguntla <manugonuguntla29@gmail.com>
2024-08-19 00:12:58 -0500

    Data Ingestionis completed and its running fine

--- start_training.py
+++ start_training.py
@@ -8,10 +8,12 @@ from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 def start_training():
     try:
-        pass
+        logging.info("Training has started")
+        model_training =TrainingPipeline()
+        model_training.run_pipeline()
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
 if __name__ == '__main__':
-    start_training
+    start_training()
     
